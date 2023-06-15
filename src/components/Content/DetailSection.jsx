b1beefca0ac9066a560ad82c9d2f3f538d04d97f
Connor Wales <connorwales@gmail.com>
2023-06-14 22:21:06 -0700

    fixed additional styling, added new images for libraries and technologies, content.json nearly complete

--- src/components/Content/DetailSection.jsx
+++ src/components/Content/DetailSection.jsx
@@ -6,16 +6,20 @@ import './DetailSection.css';
 export default function DetailSection({about}) {
   return (
     <div className="detail-section">
-      <div className="image-container">
-        <img src={about.image} />
-      </div>
+      
       <div className="text-container">
+        <div className="image-container">
+          <img src={about.image} />
+        </div>
         <div>
           <h2>{about.title}</h2>
         </div>
         <div>
           <p className="linebreak">{about.text}</p>
         </div>
+      </div>
+
+      <div className="tech-and-links">
         <div className="technologies">
           <h5>Technologies</h5>
           <div className="technologies-row">
@@ -28,25 +32,24 @@ export default function DetailSection({about}) {
               </div>
             )}
           </div>
-          
         </div>
 
         {about.links && (
-          <div className="links">
-            <h5>Links</h5>
-            <div className="links-row">
-              {about.links.map(link => 
-                <a href={link.link}>
-                  <div className="links-row-image-container">
-                    <img className="links-row-image" 
-                      title={link.name}
-                      src={link.image}
-                    />
-                  </div>
-                </a>
-              )}
-            </div>
+        <div className="links">
+          <h5>Links</h5>
+          <div className="links-row">
+            {about.links.map(link => 
+              <a href={link.link}>
+                <div className="links-row-image-container">
+                  <img className="links-row-image" 
+                    title={link.name}
+                    src={link.image}
+                  />
+                </div>
+              </a>
+            )}
           </div>
+        </div>
         )}
       </div>
       
